c95855e2d16c6dd9686d1da15216baf5b83dc919
ducksfrogs <ma_yamaki@yahoo.com>
2023-01-16 18:15:00 +0900

    fix

--- settings/npm.sh
+++ settings/npm.sh
@@ -3,3 +3,4 @@
 npm init -y
 npm i --save-dev webpack webpack-cli webpack-dev-server
 npm i --save bootstrap @popperjs/core
+npm i --save-dev autoprefixer css-loader postcss-loader sass sass-loader style-loader
