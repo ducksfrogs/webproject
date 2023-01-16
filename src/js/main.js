d830db45c08a204acce6d2ff7111276e28831036
ducksfrogs <ma_yamaki@yahoo.com>
2023-01-16 19:25:34 +0900

    d

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import '../scss/styles.scss'
+
+import * as bootstrap from 'bootstrap'
+
+import Alert from 'bootstrap/js/dist/alert'
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,43 @@
 const path = require('path');
 
 module.exports = {
-    entry: '/src/main.js',
+    entry: '/src/js/main.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
     devServer: {
         static: path.resolve(__dirname, 'dist'),
-        port: 8080,
+        port: 8081,
         hot: true
     },
-    
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: () => [
+                                    require('autoprefixer')
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    }    
+
 }
